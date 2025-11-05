810395fabe49d58de8381c87f7e39eee2eb31b19
Louis Backstrom <44354406+Louis-Backstrom@users.noreply.github.com>
2025-11-05 10:31:38 +0000

    version 0.13.1
    Add CIs to JE10 models (from McCrea et al. 2024)

--- R/JE10F1.R
+++ R/JE10F1.R
@@ -2,17 +2,21 @@
 #'
 #' @description
 #' Equation 4 from Jarić & Ebenhard 2010. Estimates a p-value for testing
-#' competing hypotheses of extinction/non-extinction.
+#' competing hypotheses of extinction/non-extinction and a one-tailed
+#' \eqn{1 - \alpha} confidence interval on the time of extinction.
 #'
 #' @param records sighting records in `ccon` format (see
 #' \code{\link{convert_dodo}} for details).
+#' @param alpha desired significance level (defaults to \eqn{\alpha = 0.05}) of
+#' the \eqn{1 - \alpha} confidence interval.
 #' @param init.time start of the observation period. Defaults to the time of
 #' the first sighting, in which case this sighting is removed from the record.
 #' @param test.time end of the observation period, typically the present day
 #' (defaults to the current year).
 #'
-#' @returns a `list` object with the original parameters and the p-value
-#' included as elements.
+#' @returns a `list` object with the original parameters and the p-value and
+#' confidence interval included as elements. The confidence interval is a
+#' two-element numeric vector called `conf.int`.
 #'
 #' @note
 #' All sighting records are assumed to be certain and sampling effort is assumed
@@ -24,6 +28,12 @@
 #' Jarić, I., & Ebenhard, T. (2010). A method for inferring extinction based on
 #' sighting records that change in frequency over time. *Wildlife Biology*,
 #' 16(3), 267-275. \doi{10.2981/09-044}
+#'
+#' **Other References**
+#'
+#' McCrea, R. S., Cheale, T., Campillo-Funollet, E., & Roberts, D. L. (2024).
+#' Inferring species extinction from sighting data. *Cambridge Prisms: *
+#' *Extinction*, 2, e19. \doi{10.1017/ext.2024.18}
 #'
 #' @seealso [JE10F2()]
 #'
@@ -37,7 +47,7 @@
 #'
 #' @export
 
-JE10F1 <- function(records, init.time = min(records),
+JE10F1 <- function(records, alpha = 0.05, init.time = min(records),
                    test.time = as.numeric(format(Sys.Date(), "%Y"))) {
   # Sort records
   records <- sort(records)
@@ -61,12 +71,17 @@ JE10F1 <- function(records, init.time = min(records),
   p.value <- ((tn - 1) / (n - 1)) /
     (((tn - 1) / (n - 1)) + (bigT - tn))
 
+  # Calculate width of confidence interval
+  x <- tn + (((tn - 1) / (n - 1)) + 0) * ((1 - alpha) / alpha)
+
   # Output
   output <- list(
     records = records,
+    alpha = alpha,
     init.time = init.time,
     test.time = test.time,
-    p.value = p.value
+    p.value = p.value,
+    conf.int = c(init.time + tn, init.time + x)
   )
 
   return(output)
