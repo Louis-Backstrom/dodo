8e6081ea44970b2ef333f8ddeb8df40169a12856
Louis Backstrom <44354406+Louis-Backstrom@users.noreply.github.com>
2026-01-05 15:46:59 +0000

    version 1.1.3
    Add bitterweed dataset from RO09B1

--- R/data.R
+++ R/data.R
@@ -524,3 +524,19 @@
 #' variable reliability. *Journal of Applied Ecology*, 51(1), 251-258.
 #' \doi{10.1111/1365-2664.12144}
 "lee_s1"
+
+#' @title Bitterweed sightings
+#'
+#' @description
+#' Bitterweed (*Helenium amarum*) sighting record from Rout et al. (2009).
+#' Format: `cbin`.
+#'
+#' @format a `numeric` vector with 178 values.
+#'
+#' @references
+#' **Key Reference**
+#'
+#' Rout, T. M., Salomon, Y., & McCarthy, M. A. (2009). Using sighting records
+#' to declare eradication of an invasive species. *Journal of Applied Ecology*,
+#' 46(1), 110-117.\doi{10.1111/j.1365-2664.2008.01586.x}
+"bitterweed"
